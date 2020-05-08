3ae86cacefdf6c6add0611544a96ce15490688f0
zoeferencova <zoeferencova@gmail.com>
2020-05-08 12:15:58 -0400

    added client origin to cors configuration

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 
 const app = express();
 
@@ -11,7 +11,9 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'dev';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
