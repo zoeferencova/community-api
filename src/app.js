93aea370e66b2e29c6c3e116f80580c949d1f5d1
zoeferencova <zoeferencova@gmail.com>
2020-06-10 18:28:33 -0400

    added cross domain function

--- src/app.js
+++ src/app.js
@@ -19,6 +19,22 @@ app.options('*', cors())
 app.use(cors());
 const server = require('http').Server(app);
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain)
+
 // Middleware setup
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'dev';
 app.use(morgan(morganOption));
